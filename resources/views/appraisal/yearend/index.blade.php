00326391d665f49d8a928c5d8300630078ca0b52
RaianHossain <raiansanil@gmail.com>
2026-06-24 15:03:22 +0600

    print button to employee's final assessment

--- resources/views/appraisal/yearend/index.blade.php
+++ resources/views/appraisal/yearend/index.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('content')
     <div class="container-fluid py-4">
+        <div class="d-flex justify-content-end mb-3">
+            <button onclick="window.print()" class="btn btn-secondary d-print-none">
+                <i class="fas fa-print me-1"></i> Print / Preview
+            </button>
+        </div>
+
         @include('components.alert')
 
         @include('appraisal.shared.year_end_appraisal', [
